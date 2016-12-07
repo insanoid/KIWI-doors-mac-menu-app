82816271c1b400ffb1f93bfe1febdfe0ab8fac28
Karthikeya Udupa K M <karthikeyaudupa@gmail.com>
2016-12-07 20:54:43 +0100

    Initial Commit

---
+++ KIWI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KIWI
+//
+//  Created by Karthik on 07/12/2016.
+//  Copyright © 2016 Karthik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
